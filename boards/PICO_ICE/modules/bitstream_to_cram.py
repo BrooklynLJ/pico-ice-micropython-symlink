2ccd615ef9e5eca44b36dc5edfae3f02c1a51b42
Brooklyn Jennings <brooklynlj@outlook.com>
2025-07-17 15:15:31 -0400

    bruh

--- boards/PICO_ICE/modules/bitstream_to_cram.py
+++
@@ -1,6 +0,0 @@
-from machine import Pin
-import ice
-fpga = ice.fpga(cdone=Pin(ICE_DONE), clock=Pin(ICE_CLK), creset=Pin(ICE_RST), cram_cs=Pin(ICE_SS), cram_mosi=Pin(ICE_SI), cram_sck=Pin(ICE_SCK), frequency=48)
-file = open("bitstream.bin", "br")
-fpga.start()
-fpga.cram(file)
